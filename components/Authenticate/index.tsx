608d906e3b68d13b774ed611c82aa3f374e3f411
reza <mrr.reza@gmail.com>
2022-08-07 20:54:08 +0430

    added authentication page

--- components/Authenticate/index.tsx
+++ components/Authenticate/index.tsx
@@ -1,5 +1,28 @@
-const Authenticate: React.FC = () => {
-  return <input type="text" placeholder="Please Enter Personal Access Key" />;
+import { useState } from "react";
+import classes from "./authenticate.module.css";
+
+interface authenticatePropsTypes {
+  fetchCommits: (pta: string) => void;
+}
+
+const Authenticate: React.FC<authenticatePropsTypes> = ({ fetchCommits }) => {
+  const [input, setInput] = useState<string>("");
+
+  return (
+    <>
+      <div className={classes.container}>
+        <input
+          className={classes.input}
+          type="textt"
+          placeholder="Enter personal access token"
+          onChange={(e) => setInput(e.target.value)}
+        ></input>
+        <button className={classes.button} onClick={() => fetchCommits(input)}>
+          &rarr;
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Authenticate;
